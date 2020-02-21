357e06ceeaa285931f38356d62c89fc67e6f126e
WY1479052860 <1479052860@qq.com>
2020-02-21 13:35:24 +0800

    创建项目

---
+++ app/src/main/java/com/bawei/wangyi20200221/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bawei.wangyi20200221;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 这是第一次的注释
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
